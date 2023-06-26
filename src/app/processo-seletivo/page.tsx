d6b63407d61eaf11376c8dcb1078910590872ac1
Igor Rocha <igor_roc@hotmail.com.br>
2023-06-26 17:07:53 -0300

    fix: Cronograma

--- src/app/processo-seletivo/page.tsx
+++ src/app/processo-seletivo/page.tsx
@@ -1,5 +1,56 @@
 import React from "react"
 
+import styles from "./processo.module.css"
+import WaveSvg from "@/components/Svg/Wave"
+
 export default function ProcessoSeletivo() {
-	return <div>ProcessoSeletivo</div>
+	return (
+		<>
+			<section className={styles.hero}>
+				<div className={[styles.heroWrapper, "max-width"].join(" ")}>
+					<div className={styles.heroContent}>
+						<h1 className={styles.heroTitle}>Processo Seletivo</h1>
+					</div>
+				</div>
+
+				<WaveSvg className={styles.waveSvg} color="#318eb3" />
+			</section>
+
+			<section>
+				<div className={"max-width"}>
+					<h2 className={styles.title}>Cronograma</h2>
+					<div className={styles.cronograma}>
+						<div className={styles.cronogramaItem}>
+							<strong>Período de inscrições:</strong>
+							<p>05/07/2023 a 21/07/2023</p>
+						</div>
+						<div className={styles.cronogramaItem}>
+							<strong>Processo Seletivo:</strong>
+							<p>25/07/2023</p>
+						</div>
+						<div className={styles.cronogramaItem}>
+							<strong>Resultados:</strong>
+							<p>27/07/2023</p>
+						</div>
+						<div className={styles.cronogramaItem}>
+							<strong>Prazos Recursos:</strong>
+							<p>28/07/2023 a 30/07/2023</p>
+						</div>
+						<div className={styles.cronogramaItem}>
+							<strong>Resposta a Recursos:</strong>
+							<p>31/07/2023</p>
+						</div>
+						<div className={styles.cronogramaItem}>
+							<strong>Resultado Final:</strong>
+							<p>01/08/2023</p>
+						</div>
+						<div className={styles.cronogramaItem}>
+							<strong>Início das Aulas:</strong>
+							<p>07/08/2023</p>
+						</div>
+					</div>
+				</div>
+			</section>
+		</>
+	)
 }
