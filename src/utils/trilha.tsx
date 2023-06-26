819a528faad8b397f279a37a94bd5bce79c71cc3
Igor Rocha <igor_roc@hotmail.com.br>
2023-06-26 16:36:17 -0300

    fix: Remove buttons from trilha

--- src/utils/trilha.tsx
+++ src/utils/trilha.tsx
@@ -16,24 +16,19 @@ const trilha = [
 		title: "Formação Tecnológica",
 		description:
 			"Os residentes selecionados receberão treinamento em Java, Python, .Net e Desenvolvimento de Interfaces. As aulas serão presenciais e realizadas em laboratório de informática. O residente deverá reservar pelo menos 30h semanais para se dedicar à residência.",
-		link: "#",
-		link_label: "Saiba mais",
+
 		image: FormacaoIMG,
 	},
 	{
 		title: "Imersão em Desafios",
 		description:
 			"Desafios cadastrados por empresas parceiras estarão disponíveis para serem solucionados pelos residentes organizados em equipes de trabalho. As equipes serão acompanhadas por tutores experientes. Os residentes também poderão desenvolver ideias próprias",
-		link: "#",
-		link_label: "Saiba mais",
 		image: ImersaoIMG,
 	},
 	{
 		title: "Pós residência",
 		description:
 			"Ao final da residência os alunos terão tido a oportunidade de empreender a partir do desenvolvimento das suas próprias ideias, incubando empresas. Também terão sido expostos a oportunidades de seleção nas empresas parceiras. Espera-se que todos tenham melhorado seus currículos e sua experiência e se tornado assim um profissional desejado pelo mercado",
-		link: "#",
-		link_label: "Saiba mais",
 		image: PosResidenciaIMG,
 	},
 ]
