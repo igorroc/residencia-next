b4ef648cb302e6fa6e2f82c39dc1001b5dd96248
Igor Rocha <igor_roc@hotmail.com.br>
2023-07-14 09:33:52 -0300

    fix: New cronograma

--- src/utils/cronograma.ts
+++ src/utils/cronograma.ts
@@ -1,35 +1,35 @@
 const cronograma = [
 	{
 		title: "Inscrições:",
-		date: "12 a 24 de julho",
+		date: "17 a 29 de julho",
 	},
 	{
 		title: "Processo seletivo:",
-		date: "26 de julho",
-	},
-	{
-		title: "Resultados:",
-		date: "28 de julho",
-	},
-	{
-		title: "Prazos para Recursos:",
-		date: "29 a 31 de julho",
-	},
-	{
-		title: "Resposta aos Recursos:",
 		date: "01 de agosto",
 	},
 	{
+		title: "Resultados:",
+		date: "03 de agosto",
+	},
+	{
+		title: "Prazos para Recursos:",
+		date: "04 a 06 de agosto",
+	},
+	{
+		title: "Resposta aos Recursos:",
+		date: "07 de agosto",
+	},
+	{
 		title: "Resultado Final:",
-		date: "02 de agosto",
+		date: "07 de agosto",
 	},
 	{
 		title: "Período de Matrícula:",
-		date: "03 a 04 de agosto",
+		date: "08 a 09 de agosto",
 	},
 	{
 		title: "Início das Aulas (previsto):",
-		date: "09 de agosto",
+		date: "10 de agosto",
 	},
 ]
 
