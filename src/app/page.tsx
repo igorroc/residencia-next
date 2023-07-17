8b7ec2358df0d62f5bdd27b4f2a3f50134445c30
Igor Rocha <igor_roc@hotmail.com.br>
2023-07-17 14:13:16 -0300

    feat: Google forms link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -117,7 +117,7 @@ export default function Home() {
 								Eletroeletrônica de Ilhéus (CEPEDI), em parceria com a Universidade
 								Estadual de Santa Cruz (UESC).
 							</p>
-							<CustomLink href="/processo-seletivo" label="Inscreva-se" />
+							<CustomLink href="https://docs.google.com/forms/d/e/1FAIpQLSeOcYRlOrIM2D7McbLLQve2rhvJsdiONvLOKjcy6d3id_B19A/formResponse" label="Inscreva-se" />
 						</div>
 						<div className={styles.sobreImage}>
 							{/* eslint-disable-next-line @next/next/no-img-element */}
